def58796d36f5e3b127f6f67e640c824cebf4d15
AJakif <akifahmedjahin@gmail.com>
2021-11-01 14:21:27 +0600

    Prime react added, Checkbox not working

--- a2z/src/components/pages/auth/login/Login.tsx
+++ a2z/src/components/pages/auth/login/Login.tsx
@@ -1,11 +1,16 @@
 import "./login.css";
-import { InputText } from 'primereact/inputtext';
+import { InputText } from "primereact/inputtext";
+import { Checkbox } from "primereact/checkbox";
 import logo from "../../../../assets/Logo/Logo.png";
 import Image from "../../../../assets/Image/Login.png";
 import Facebook from "../../../../assets/Icons/facebook.svg";
 import Google from "../../../../assets/Icons/google.svg";
+import { useState } from "react";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [pass, setPass] = useState("");
+  const [checked, setChecked] = useState(false);
   return (
     <div>
       <div className="container">
@@ -25,46 +30,43 @@ function Login() {
             <div className="head">
               <h1>Login your account</h1>
             </div>
-            <span className="p-float-label">
-                    <InputText id="username"  />
-                    <label htmlFor="username">Username</label>
-                </span>
-
-            <Form>
-              <Form.Item
-                name="remember"
-                valuePropName="checked"
-                wrapperCol={{ offset: 0, span: 12 }}
-              >
-                <Checkbox>Remember me</Checkbox>
-              </Form.Item>
-              <Form.Item wrapperCol={{ offset: 6, span: 16 }}>
-                <Button type="primary">Login</Button>
-              </Form.Item>
-            </Form>
-            <Form>
-              <Row>
-                <Col span={12}>
-                  {" "}
-                  <a href="/">Forget Password?</a>
-                </Col>
-                <Col className="rightallign" span={12}>
-                  <a href="/">Creat Account</a>
-                </Col>
-              </Row>
-            </Form>
+            <div className="form">
+              <div className="email">
+                <p>Username or Email</p>
+                <InputText
+                  id="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+              <div className="pass">
+                <p>Password</p>
+                <InputText
+                  id="pass"
+                  value={pass}
+                  onChange={(e) => setPass(e.target.value)}
+                />
+              </div>
+              <div className="p-field-checkbox">
+                <Checkbox
+                  inputId="remember"
+                  checked={checked}
+                  onChange={(e) => setChecked(e.checked)}
+                />
+                <label htmlFor="remember">Remember me</label>
+              </div>
+            </div>
             <div className="line-container">
               <div className="line"></div>
               <p>Or Direct Login With</p>
               <div className="icons">
-              <a href="www.facebook.com">
-               <img src={Facebook} alt="facebook" />
-              </a>
-              <a href="www.google.com">
-                <img src={Google} alt="google" />
-              </a>
+                <a href="www.facebook.com">
+                  <img src={Facebook} alt="facebook" />
+                </a>
+                <a href="www.google.com">
+                  <img src={Google} alt="google" />
+                </a>
               </div>
-              
             </div>
           </div>
         </div>
